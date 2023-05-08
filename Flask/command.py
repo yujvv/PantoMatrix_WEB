572c5f7932ee1cb288a576304c83701d1d172e15
yujvv <106058346+yujvv@users.noreply.github.com>
2023-05-08 13:59:42 +0900

    run in

--- Flask/command.py
+++ Flask/command.py
@@ -2,12 +2,21 @@ import subprocess
 import time
 
 def run_commands():
-    first_command = 'cd /home/ubuntu/Data/Demo&&python synthesize.py'
-    subprocess.run(first_command, shell=True)
+    # first_command = 'cd /home/ubuntu/Data/Demo&&python synthesize.py'
+    # subprocess.run(first_command, shell=True)
+
+    env_name = 'aaa'
+    first_command = ['python', '/home/ubuntu/Data/Demo/synthesize.py']
+    subprocess.run(['conda', 'run', '-n', env_name] + first_command, check=True)
+
     
     # 等待 1 秒钟，或者等待第一个命令完成后，再运行第二个命令
     time.sleep(1)
 
-    second_command = 'cd /home/ubuntu/codes/audio2pose&&python test_wild.py -c ./configs/test_wild.yaml'
-    subprocess.run(second_command, shell=True)
+    # second_command = 'cd /home/ubuntu/codes/audio2pose&&python test_wild.py -c ./configs/test_wild.yaml'
+    # subprocess.run(second_command, shell=True)
+
+    env_name = 'bbb'
+    second_command = ['python', '/home/ubuntu/codes/audio2pose/test_wild.py', '-c', './configs/test_wild.yaml']
+    subprocess.run(['conda', 'run', '-n', env_name] + second_command, check=True)
 
